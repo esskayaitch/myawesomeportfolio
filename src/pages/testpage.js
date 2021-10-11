f740bbb20bcbd9e4092d9a740821b67a814ee90f
esskayaitch <simonhillskh@gmail.com>
2021-10-11 22:06:23 +1100

    Completed Lesson 16

--- src/pages/testpage.js
+++ src/pages/testpage.js
@@ -4,13 +4,13 @@ import { Link } from "gatsby"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
-const SecondPage = () => (
+const TestPage = () => (
   <Layout>
     <SEO title="Page two" />
-    <h1>Hi from the second page</h1>
-    <p>Welcome to page 2</p>
+    <h1>Hi from the test page</h1>
+    <p>Welcome to the test page</p>
     <Link to="/">Go back to the homepage</Link>
   </Layout>
 )
 
-export default SecondPage
+export default TestPage
